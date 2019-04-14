2bd597a202aae94b15ccd4b5f5f9a9502372b28e
Zeth <38241145+zeth1998v@users.noreply.github.com>
2019-04-14 17:17:37 +0200

    Add files via upload

--- assets/js/myhub.js
+++ assets/js/myhub.js
@@ -1,7 +1,19 @@
-var counterTxt = document.querySelector("#counterTxt");
-var coin = document.querySelector("#coinBtn"), counter = 0;
+let counterTxt = document.querySelector("#counterTxt");
+let coin = document.querySelector("#coinBtn");
+let counter = 0;
+let dateTime = new Date;
+let clock = dateTime.getHours()+":"+dateTime.getMinutes()+":"+dateTime.getSeconds();
+let timeDisplay = document.querySelector("#timeDisplay");
+let dateTimeInterval = setInterval(myDateTime,1000);
+
+timeDisplay.innerHTML = clock;
 
 coin.onclick = function(){
-	counter += 1;
+	counter++;
 	counterTxt.innerHTML = counter;
-};
+}
+function myDateTime() {
+	let time = new Date;
+	timeStr = time.getHours()+":"+time.getMinutes()+":"+time.getSeconds();
+	timeDisplay.innerHTML = timeStr;
+}
--- myhub.php
+++ myhub.php
@@ -16,6 +16,9 @@
 	<link href="https://fonts.googleapis.com/css?family=Raleway" rel="stylesheet">
 </head>
 <body>
+	<div id="navBar">
+		<p id="timeDisplay"></p>
+	</div>
 	<div class="counter">
 		<h1 id="counterTxt">0</h1>
 		
@@ -25,6 +28,13 @@
 	</div>
 	<div class="coin">
 		<button id="coinBtn"><img src="http://pngimg.com/uploads/coin/coin_PNG36871.png"></button>
+	</div>
+	<div id="flowMenu">
+		<ul>
+			<li><button id="flowBtn_1"><img src=""><h1>Lemon Stand</h1><p>$50</p></button></li>
+			<li><button id="flowBtn_2"><img src=""><h1>Newspaper Delivery</h1><p>$1,000</p></button></li>
+			<li><button id="flowBtn_3"><img src=""><h1>Car Wash</h1><p>$75,000</p></button></li>
+		</ul>
 	</div>
 </body>
 <script type="text/javascript" src="assets/js/myhub.js"></script>
